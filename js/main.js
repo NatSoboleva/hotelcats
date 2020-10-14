4c838a925623aa1ad7df232d9ebd94463392012e
NatSoboleva <nsoboleva734@gmail.com>
2020-10-14 10:42:50 +0300

    add block reviews

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var mySwiper = new Swiper('.swiper-container', {
+var roomsSlider = new Swiper('.rooms-slider', {
   // Optional parameters
   // direction: 'vertical',
   loop: true,
@@ -10,8 +10,26 @@ var mySwiper = new Swiper('.swiper-container', {
 
   // Navigation arrows
   navigation: {
-    nextEl: '.slider-button--next',
-    prevEl: '.slider-button--prev',
+    nextEl: '.rooms-slider__button--next',
+    prevEl: '.rooms-slider__button--prev',
+  },
+
+});
+
+var reviewsSlider = new Swiper('.swiper-reviews', {
+  // Optional parameters
+  // direction: 'vertical',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.reviews-slider__button--next',
+    prevEl: '.reviews-slider__button--prev',
   },
 
 })
